9dcb339b4ef3be49e1b6de8f60ea21e78457937d
desideri <mr.desipeli@gmail.com>
2022-04-01 17:35:27 +0300

    refaktorointia ja uusi testi

--- src/tests/deck_test.py
+++ src/tests/deck_test.py
@@ -12,25 +12,27 @@ class TestDeck(unittest.TestCase):
         self.deck = Deck(self.cards, "test")
 
     
-    def test_pick_top_card(self):
-        self.assertEqual((self.deck.pick_top().v_hand, self.deck.pick_top().suit), (13, "3"))
-
     def test_deck_diminishes(self):
-        for i in range(10):
-            self.deck.pick_top()
+        self.pick_n(10)
         self.assertEqual(len(self.deck.see_deck()), 42)
     
     def test_deck_get_back(self):
         self.assertEqual(self.deck.get_back(), "test")
     
     def test_see_full_deck(self):
-        for i in range(10):
-            self.deck.pick_top()
+        self.pick_n(10)
         self.assertEqual(len(self.deck.see_full_deck()), 52)
     
     def test_assemble_deck(self):
-        for i in range(10):
-            self.deck.pick_top()
+        self.pick_n(10)
         self.deck.assemble_deck()
         self.assertEqual(len(self.deck.see_deck()), 52)
+    
+    def test_pick_when_empty(self):
+        self.pick_n(55)
+        self.assertEqual(len(self.deck.see_deck()), 0)
+    
+    def pick_n(self, n):
+        for i in range(n):
+            self.deck.pick_top()
 
