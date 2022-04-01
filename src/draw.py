4243419549c434498b529187acd49fca7f35b137
desideri <mr.desipeli@gmail.com>
2022-04-01 16:24:23 +0300

    draw_deck piirtää nyt matsiolion pakan

--- src/draw.py
+++ src/draw.py
@@ -19,7 +19,8 @@ class Draw:
     def draw_deck(self):
         #for i in range(len(self.info.deck.see_full_deck())):
             #self.info.screen.blit(self.info.backs[0].image, (30, self.info.screen.get_height()/2 - self.info.backs[0].image.get_height()/2))
-        self.info.screen.blit(self.info.backs[0].image, self.info.deck_pos)
+        if self.info.match:
+            self.info.screen.blit(self.info.match.deck.get_back().image, self.info.deck_pos)
 
         #Draw all cards test
         #if self.info.game_stage == 1:
