c2c47f5cb8a4b6d5127c156c5b19d92342f69091
desideri <mr.desipeli@gmail.com>
2022-04-01 16:26:34 +0300

    matsin ja siihen liitetyn pakan alustus

--- src/event_handler.py
+++ src/event_handler.py
@@ -1,5 +1,7 @@
 import pygame
 import sys
+from match import Match
+from deck import Deck
 
 class Events:
 
@@ -19,17 +21,27 @@ class Events:
             for b in self.info.menu_buttons:
                 if self.check_button(b):
                     if b.text == "Play":
-                        # game stage = 1
-                        print("play")
-                        self.info.game_stage = 1
-                        pass
+                        self.start_match()
         
         # Game tabel
-        if self.info.game_stage == 1:
-            pass
+        elif self.info.game_stage == 1:
+            self.check_click_deck()
 
     def check_button(self, button):
         if self.event.pos[0] >= button.center[0] and self.event.pos[0] <= button.center[0]+button.size[0]:
             if self.event.pos[1] >= button.center[1] and self.event.pos[1] <= button.center[1]+button.size[1]:
                 return True
         return False
+
+    def check_click_deck(self):
+        if self.event.pos[0] >= self.info.deck_pos[0] and self.event.pos[0] <= self.info.deck_pos[0] + self.info.match.deck.get_back().image.get_width():
+            if self.event.pos[1] >= self.info.deck_pos[1] and self.event.pos[1] <= self.info.deck_pos[1] + self.info.match.deck.get_back().image.get_height():
+                print("Deck!")
+
+    def start_match(self):
+        print("Match started!")
+        match = Match(self.info)
+        match.deck = Deck(self.info.cards.copy(), self.info.backs[0])
+        self.info.match = match
+        self.info.game_stage = 1
+        
