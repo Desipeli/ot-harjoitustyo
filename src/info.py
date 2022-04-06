9275eb6b3caff4a0073d6e1ac2d36169803dc187
desideri <mr.desipeli@gmail.com>
2022-04-06 10:47:34 +0300

    Lisätty lista pelinaikaisille napeille

--- src/info.py
+++ src/info.py
@@ -9,6 +9,7 @@ class Info:
         self.cards = []
         self.backs = []
         self.menu_buttons = []
+        self.game_buttons = []
         self.deck_pos = (0,0)
         self.match = None
 
