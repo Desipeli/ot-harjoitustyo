ddcda5197b1a699379bd6f4150280c943c26bd0e
desideri <mr.desipeli@gmail.com>
2022-04-06 08:34:50 +0300

    Erän aloitusjaot

--- src/match.py
+++ src/match.py
@@ -12,3 +12,21 @@ class Match:
         self.round = 1
         self.points_player = 0
         self.points_computer = 0
+        
+    def start_round(self):
+        self.deal_cards(True)
+    
+    def deal_cards(self, start_of_round):
+        for i in range(2):
+            if self.round % 2 == 1: # Start by dealing player first
+                self.deal_two_cards_to(self.player_hand)
+                self.deal_two_cards_to(self.computer_hand)
+                self.deal_two_cards_to(self.table)
+            else:
+                self.deal_two_cards_to(self.computer_hand)
+                self.deal_two_cards_to(self.player_hand)               
+                self.deal_two_cards_to(self.table)
+    
+    def deal_two_cards_to(self, target):
+        target.append(self.deck.pick_top())
+        target.append(self.deck.pick_top())
