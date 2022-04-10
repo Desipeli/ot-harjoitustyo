5ab07af2e5042fb49e0b1fada94ced1ed273d5ca
desideri <mr.desipeli@gmail.com>
2022-04-10 10:26:54 +0300

    Testien ajaminen toimii komennolla poetry run invoke coverage-report

--- tasks.py
+++ tasks.py
@@ -2,5 +2,13 @@ from invoke import task
 
 
 @task
-def start (ctx):
+def start(ctx):
     ctx.run("python3 src/index.py", pty=True)
+
+@task
+def coverage(ctx):
+    ctx.run("coverage run --branch -m pytest src", pty=True)
+
+@task(coverage)
+def coverage_report(ctx):
+    ctx.run("coverage html", pty=True)
