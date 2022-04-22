55f2433e2aef73ae7eef3ba923d3780a5b8684ff
desideri <mr.desipeli@gmail.com>
2022-04-22 14:15:43 +0300

    Testejä valittujen korttien nostoon

--- src/tests/combinations_test.py
+++ src/tests/combinations_test.py
@@ -8,10 +8,82 @@ class TestCalc(unittest.TestCase):
         self.cards = [
             Card(14, 1, "spades", "image"),
             Card(14, 1, "hearts", "image"),
-            Card(16, 10, "diamonds", "image"),
-            Card(13, 13, "spades", "image"),
+            Card(6, 6, "diamonds", "image"),
+            Card(8, 8, "spades", "image"),
         ]
     
     def test_table_combinations_count(self):
-        result = self.calc.find_all_table_combinations(self.cards)
+        result = self.calc.find_all_table_combinations(self.cards, [])
         self.assertEqual(len(result), 16)
+
+    def test_check_if_pick_is_allowed_yes_2_pairs(self):
+        book = {}
+        card = Card(8, 8, "hearts", "image")
+        checked = [0 for x in range(len(self.cards))]
+        self.assertEqual(self.calc.check_if_pick_is_allowed(self.cards, card, checked, 0, 0, book), True)
+    
+    def test_check_if_pick_is_allowed_yes_all_same(self):
+        book = {}
+        cards = [
+            Card(3, 3, "spades", "image"),
+            Card(3, 3, "hearts", "image"),
+            Card(3, 3, "diamonds", "image"),
+        ]
+        card = Card(3, 3, "hearts", "image")
+        checked = [0 for x in range(len(cards))]
+        self.assertEqual(self.calc.check_if_pick_is_allowed(cards, card, checked, 0, 0, book), True)
+
+    def test_check_if_pick_is_allowed_yes_all_different(self):
+        book = {}
+        cards = [
+            Card(2, 2, "spades", "image"),
+            Card(3, 3, "hearts", "image"),
+            Card(4, 4, "diamonds", "image"),
+        ]
+        card = Card(9, 9, "hearts", "image")
+        checked = [0 for x in range(len(cards))]
+        self.assertEqual(self.calc.check_if_pick_is_allowed(cards, card, checked, 0, 0, book), True)
+
+    def test_check_if_pick_is_allowed_no_all_same(self):
+        book = {}
+        cards = [
+            Card(3, 3, "spades", "image"),
+            Card(3, 3, "hearts", "image"),
+            Card(3, 3, "diamonds", "image"),
+        ]
+        card = Card(4, 4, "hearts", "image")
+        checked = [0 for x in range(len(cards))]
+        self.assertEqual(self.calc.check_if_pick_is_allowed(cards, card, checked, 0, 0, book), False)
+    
+    def test_check_if_pick_is_allowed_no_all_different(self):
+        book = {}
+        cards = [
+            Card(2, 2, "spades", "image"),
+            Card(3, 3, "hearts", "image"),
+            Card(4, 4, "diamonds", "image"),
+        ]
+        card = Card(10, 10, "hearts", "image")
+        checked = [0 for x in range(len(cards))]
+        self.assertEqual(self.calc.check_if_pick_is_allowed(cards, card, checked, 0, 0, book), False)
+    
+    def test_check_if_pick_is_allowed_no_one_allowed_pair(self):
+        book = {}
+        cards = [
+            Card(2, 2, "spades", "image"),
+            Card(3, 3, "hearts", "image"),
+            Card(4, 4, "diamonds", "image"),
+        ]
+        card = Card(5, 5, "hearts", "image")
+        checked = [0 for x in range(len(cards))]
+        self.assertEqual(self.calc.check_if_pick_is_allowed(cards, card, checked, 0, 0, book), False)
+    
+    def test_check_if_pick_is_allowed_no_one_same_card(self):
+        book = {}
+        cards = [
+            Card(9, 9, "spades", "image"),
+            Card(3, 3, "hearts", "image"),
+            Card(4, 4, "diamonds", "image"),
+        ]
+        card = Card(9, 9, "hearts", "image")
+        checked = [0 for x in range(len(cards))]
+        self.assertEqual(self.calc.check_if_pick_is_allowed(cards, card, checked, 0, 0, book), False)
