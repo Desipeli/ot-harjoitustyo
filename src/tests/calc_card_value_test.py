815ed2925ee7f4c3f9f587e8da595e27463cb152
desideri <mr.desipeli@gmail.com>
2022-04-22 10:12:03 +0300

    Korjattu virheelliset arvot

--- src/tests/calc_card_value_test.py
+++ src/tests/calc_card_value_test.py
@@ -5,6 +5,7 @@ from card import Card
 
 class TestCalc(unittest.TestCase):
     def setUp(self):
+        self.card_base = 0.1
         self.ace = 1
         self.s2 = 1
         self.spades = 0.2
@@ -21,28 +22,28 @@ class TestCalc(unittest.TestCase):
         ]
     
     def test_card_value_ace_of_spades(self):
-        self.assertEqual(self.calc.card_value(self.cards[0], []), self.ace + self.spades)
+        self.assertEqual(self.calc.card_value(self.cards[0], []), self.ace + self.spades + self.card_base)
     
     def test_card_value_ace(self):
-        self.assertEqual(self.calc.card_value(self.cards[1], []), self.ace)
+        self.assertEqual(self.calc.card_value(self.cards[1], []), self.ace + self.card_base)
     
     def test_card_value_d10(self):
-        self.assertEqual(self.calc.card_value(self.cards[2], []), self.d10)
+        self.assertEqual(self.calc.card_value(self.cards[2], []), self.d10 + self.card_base)
     
     def test_card_value_spades(self):
-        self.assertEqual(self.calc.card_value(self.cards[3], []), self.spades)
+        self.assertEqual(self.calc.card_value(self.cards[3], []), self.spades + self.card_base)
 
     def test_card_value_basic(self):
-        self.assertEqual(self.calc.card_value(self.cards[4], []), 0)
+        self.assertEqual(self.calc.card_value(self.cards[4], []), self.card_base)
     
     def test_card_value_spades_when_player_has_half(self):
-        self.assertEqual(self.calc.card_value(self.cards[3], self.p_col), 0)
+        self.assertEqual(self.calc.card_value(self.cards[3], self.p_col), self.card_base)
     
     def test_card_value_ace_of_spades_when_player_has_half(self):
-        self.assertEqual(self.calc.card_value(self.cards[0], self.p_col), 1)
+        self.assertEqual(self.calc.card_value(self.cards[0], self.p_col), 1 + self.card_base)
     
     def test_card_value_2_of_spades(self):
-        self.assertEqual(self.calc.card_value(self.cards[5], []), self.s2 + self.spades)
+        self.assertEqual(self.calc.card_value(self.cards[5], []), self.s2 + self.spades + self.card_base)
     
     def test_card_value_2_of_spades_when_player_hald(self):
-        self.assertEqual(self.calc.card_value(self.cards[5], self.p_col), self.s2)
+        self.assertEqual(self.calc.card_value(self.cards[5], self.p_col), self.s2 + self.card_base)
