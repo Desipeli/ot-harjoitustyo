1460e54d2b20fef17166314bfe147be20df73742
desideri <mr.desipeli@gmail.com>
2022-05-03 22:20:03 +0300

    Docstring ja settings-olio

--- src/info.py
+++ src/info.py
@@ -1,6 +1,9 @@
+from settings import Settings
 
 
 class Info:
+    """ Stores information of the app """
+
     def __init__(self):
         self.screen = None
         self.font = None
@@ -10,5 +13,9 @@ class Info:
         self.menu_buttons = []
         self.game_buttons = []
         self.match_end_buttons = []
+        self.settings_buttons = []
         self.deck_pos = (0, 0)
         self.match = None
+        self.game_log_text = ["Welcome to Kasino!", "Press play to start a match"]
+        self.game_log = None
+        self.settings = Settings()
