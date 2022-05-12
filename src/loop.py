9a68e57fa1fbb6c0f83f67e2d71dea7cc6f64eb7
desideri <mr.desipeli@gmail.com>
2022-05-12 17:30:33 +0300

    Poistettu testaukseen liittyviä rivejä

--- src/loop.py
+++ src/loop.py
@@ -20,14 +20,9 @@ class Loop:
 
     def loop(self):
         while True:
-            # Events
             self.events()
             self.info.screen.fill((0, 81, 44))
             self.draw.draw()
-            # Game stage testing
-            text_game_stage = self.info.font.render(
-                f"Game stage: {self.info.game_stage}", True, (0, 0, 0))
-            self.info.screen.blit(text_game_stage, (10, 10))
             pygame.display.flip()
             self.clock.tick(60)
 
